1c31b7aa77cfa6124140b4564d683892f66547d3
Thomas-Parker24 <thomys65.ta@gmail.com>
2025-05-05 18:30:26 -0500

    [ADD] Terraform deployment has been added to project. It Works!

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "azurerm" {
   features {}
+  subscription_id = "6a85bb25-6ff1-44ee-ac00-67c96236e70d"
+  tenant_id = "ca3f1d6b-fd1f-40b1-b41a-488b980e9f7f"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -21,6 +23,45 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-linux"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "public-ip-linux"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1"]
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "nic-linux"
   location            = azurerm_resource_group.rg.location
@@ -34,25 +75,36 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_public_ip" "public_ip" {
-  name                = "public-ip-linux"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${path.module}/id_rsa"
+  file_permission = "0600"
 }
 
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                  = "linux-vm"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B1s"
+  size                  = "Standard_B2s"
   admin_username        = "azureuser"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub") # asegúrate de tener tu clave SSH aquí
+    public_key = tls_private_key.ssh_key.public_key_openssh
   }
+
+  custom_data = filebase64("./VMinit.yml")
 
   os_disk {
     caching              = "ReadWrite"
@@ -61,8 +113,8 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20_04-lts"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
     version   = "latest"
   }
 }
